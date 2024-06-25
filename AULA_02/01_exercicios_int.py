e18feafd9a74ed019552ecf86e42405208075e8a
Iasmim Horrana <horranaiasmim@gmail.com>
2024-06-25 20:42:58 -0300

    update

--- AULA_02/01_exercicios_int.py
+++ AULA_02/01_exercicios_int.py
@@ -1,35 +1,35 @@
 # Exercícios 
 # Inteiros (int)
 
-#Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# 1 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 """
 num_01 = int(input("Digite o primeiro número: "))
 num_02 = int(input("Digite o segundo número: "))
 soma = num_01 + num_02
 print("Total: {}" .format(soma))
 """
-#Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# 2 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 """
 numero_usuario = int(input("Digite um número: "))
 resto_divisao = 5
 resultado = numero_usuario % resto_divisao
 print("O resto da divisão do {} por 5 é {}: " .format(numero_usuario, resultado))
 """
-#Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 """
 numero1 = int(input("Digite um número: "))
 numero2 = int(input("Digite um segundo número: "))
 mult = numero1 * numero2
 print("A multiplicação de {} por {} é: {}" .format(numero1,numero2,mult))
 """
-#Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 """
 numero1 = int(input("Digite um número: "))
 numero2 = int(input("Digite outro número: "))
 div = numero1 // numero2
 print("A divisão inteira é: {}" .format(div))
 """
-#Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+# 5 - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 """
 numero_usuario = int(input("Digite um número: "))
 quadrado = numero_usuario * numero_usuario
---
+++ AULA_02/02_exercicios_float.py
@@ -0,0 +1,36 @@
+# Exercícios 
+# Números de Ponto Flutuante (float)
+
+# 1 - Escreva um programa que receba dois números flutuantes e realize sua adição.
+"""
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+adicao = num1 + num2
+print("O número {} + {} é: {}" .format(num1, num2, adicao))
+"""
+# 2 - Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+"""
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+media = (num1 + num2) / 2
+print("A média é: {}" .format(media))
+"""
+# 3 - Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+"""
+base = float(input("Digite um número: "))
+expoente  = float(input("Digite o expoente: "))
+resultado = base ** expoente
+print("O expoente é: {}" .format(resultado))
+"""
+# 4 - Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+"""
+graus = float(input("Digite temperatura em grau Celsius: "))
+Fahrenheit = (graus * 1.8) + 32
+print("A temperatura {} em Fahrenheit é: {}" .format(graus,Fahrenheit))
+"""
+# 5 - Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+"""
+raio = float(input("Digite o raio como entrada: "))
+area_circulo = raio ** 2
+print("A area do circulo é: {}" .format(area_circulo))
+"""
