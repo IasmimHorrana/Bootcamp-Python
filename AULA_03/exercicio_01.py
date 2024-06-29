39c9fce5a83520fc4698b39936189f527855ced1
Iasmim Horrana <horranaiasmim@gmail.com>
2024-06-29 10:52:38 -0300

    update aula 3

---
+++ AULA_03/exercicio_01.py
@@ -0,0 +1,16 @@
+"""
+Exercício 1: Verificação de Qualidade de Dados
+Você está analisando um conjunto de dados de vendas e precisa garantir que todos os registros tenham valores positivos para `quantidade` e `preço`. 
+Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos forem positivos ou "Dados inválidos" caso contrário.
+"""
+try:
+    quantidade = float(input("Digite a quantidade: "))
+    preco = float(input("Digite o preço: "))
+
+    if quantidade > 0 and preco > 0:
+        print("Dados válidos")
+    else:
+        print("Dados inválidos")
+
+except ValueError:
+    print("Erro: Por favor, insira números válidos.")
