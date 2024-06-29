0d60dc97832574c2b26dac69bbec18343933e36b
Iasmim Horrana <horranaiasmim@gmail.com>
2024-06-29 12:31:08 -0300

    update dos exercicios aula 03

--- AULA_03/exercicio_02.py
+++ AULA_03/exercicio_02.py
@@ -1,5 +1,20 @@
 """
 Exercício 2: Classificação de Dados de Sensor
 Imagine que você está trabalhando com dados de sensores IoT. Os dados incluem medições de temperatura. Você precisa 
-classificar cada leitura como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+classificar cada leitura como 'Baixa', 'Normal' ou 'Alta'. 
 """
+try:
+    temperatura = float(input("Digite a temperatura: "))
+
+    if temperatura < 15:
+        print("Temperatura baixa.")
+    elif 15 <= temperatura <= 25:
+        print("Temperatura normal.")
+    elif temperatura > 25:
+        print("Temperatura alta.")
+    else:
+        print("Dados inválidos")
+
+except ValueError:
+    print("Erro: Por favor, insira números válidos.")  
+    
---
+++ AULA_03/exercicio_04.py
@@ -0,0 +1,30 @@
+""" Exercício 4: Validação de Dados de Entrada
+Antes de processar os dados de usuários em um sistema de recomendação, você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha fornecido 
+um email válido. Escreva um programa que valide essas condições e imprima "Dados de usuário válidos" ou o erro específico encontrado.
+"""
+
+import re  # Importa o módulo de expressões regulares
+
+# Validação da idade
+while True:
+    entrada_idade = input("Digite sua idade: ")
+    try:
+        idade = int(entrada_idade)
+        if not 18 <= idade <= 65:
+            print("Idade fora do intervalo permitido (18-65 anos).")
+        else:
+            print(f"Você digitou uma idade válida: {idade}")
+            break
+    except ValueError:
+        print("Entrada inválida. Por favor, digite um número inteiro.")
+
+# Validação do e-mail
+while True:
+    entrada_email = input("Digite um e-mail: ")
+    # Utiliza re.match() para verificar se a entrada corresponde ao padrão de um e-mail válido
+    if re.match(r"[^@]+@[^@]+\.[^@]+", entrada_email):
+        print(f"Você digitou um e-mail válido: {entrada_email}")
+        break
+    else:
+        print("Entrada inválida. Por favor, digite um e-mail válido.")
+
