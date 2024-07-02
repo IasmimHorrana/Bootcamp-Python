04c09eb40e373d95f4fb9a7b6a32f53c53d245de
Iasmim Horrana <horranaiasmim@gmail.com>
2024-07-02 09:08:50 -0300

    update aula 06

---
+++ AULA_03/exercicio_06.py
@@ -0,0 +1,15 @@
+""" Exercício 6 - Contagem de Palavras em Textos
+Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
+"""
+
+texto = "Don't sit down and wait for the opportunities to come. Get up and make them."
+palavras = texto.split()
+contagem_palavras = {}
+
+for palavra in palavras:
+    if palavra in contagem_palavras:
+        contagem_palavras[palavra] += 1
+    else:
+        contagem_palavras[palavra] = 1
+
+print(contagem_palavras)
