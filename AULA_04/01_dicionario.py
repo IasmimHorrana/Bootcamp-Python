0acd72f78eb694d308aef661cf5a014631a6510e
Iasmim Horrana <horranaiasmim@gmail.com>
2024-07-19 22:51:45 -0300

    update-python

---
+++ AULA_04/01_dicionario.py
@@ -0,0 +1,11 @@
+#Crie um dicionario para armazenar informações de um livro, incluindo titulo, autor e ano de publicação.
+
+livro = {
+    'titulo': 'Beren and Luthien',
+    'autor': 'J.R.R Tolkien',
+    'ano_publicacao': 2017
+}
+
+print(f"Título: {livro['titulo']}")
+print(f"Autor: {livro['autor']}")
+print(f"Ano de Publicação: {livro['ano_publicacao']}")
